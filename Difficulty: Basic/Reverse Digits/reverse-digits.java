3ee20df001166ee0a456959c3c8eea36e5cebd1e
Mohith <mohithkumar4225@gmail.com>
2026-08-18 11:28:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse Digits/reverse-digits.java
+++ Difficulty: Basic/Reverse Digits/reverse-digits.java
@@ -1,15 +1,15 @@
 class Solution {
     public int reverseDigits(int n) {
         // Code here
-        if(n==0){
+        if(n == 0){
             return n;
         } 
         int revnum = 0;
         while(n>0){
-            int last = n % 10;
-            revnum = (revnum * 10) + last;
+            int lastdigit = n % 10;
+            revnum = (revnum * 10) + lastdigit;
             n = n/10;
-        } 
+        }
         return revnum;
     }
 }
